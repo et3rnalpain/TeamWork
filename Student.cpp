b533e184628258ad4d0acae894edea347b2d8771
invonzzz <serezha.simonin@yandex.ru>
2023-04-08 11:24:12 +0300

    begin

--- Student.cpp
+++ Student.cpp
@@ -1,11 +1,31 @@
+#include <iostream>
 #include "Student.h"
-
-void StudentAuthorization()
+#include "Teacher.h"
+using namespace std;
+int StudentAuthorization()
 {
+
+	return 1;
 }
 
 void StudentMenu()
 {
+	if (StudentAuthorization() > 0)
+	{
+		int ch;
+		cout << "Меню" << endl;
+		cout << "1. Тренинг по теме" << endl;
+		cout << "2. Тестирование по теме" << endl;
+		cout << "3. Итоговый тест" << endl;
+		cout << "Введите пункт меню: "; cin >> ch;
+		switch (ch)
+		{
+		case 1: TrainingMode(); break;
+		case 2: TestingMode(); break;
+		case 3: ExaminationMode(); break;
+		default: cout << "Неверный пункт меню";
+		}
+	}
 }
 
 void TrainingMode()
--- Student.h
+++ Student.h
@@ -1,6 +1,6 @@
 #pragma once 
 
-void StudentAuthorization();
+int StudentAuthorization();
 
 void StudentMenu();
 
