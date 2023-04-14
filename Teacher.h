ab050198dac133dbf3cea38c49a2cd314404964c
stereob1tch <nikita.dolb@gmail.com>
2023-04-14 22:23:20 +0300

    upload

--- Teacher.h
+++ Teacher.h
@@ -1,9 +1,8 @@
 #pragma once 
 #include "FileModule.h"
-void StudentListChange();
-void QuestionDelete(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
-void QuestionAdd(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
+void StudentListChange(user users[]);
+void QuestionDelete(int theme_num, question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
+void QuestionAdd(int theme_num, question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
 void QuestionWork(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
-void QuestionChange(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
-void QuestListOutput(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
+void QuestionChange(int theme_num, question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
 void TeacherMenu(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[]);
