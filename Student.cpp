f2f82ac62ef74ac26147ffb9c30a220d98d38e2d
et3rnalpain <gjobovi4@mail.ru>
2023-04-14 20:38:09 +0300

    update

--- Student.cpp
+++ Student.cpp
@@ -6,15 +6,8 @@
 #include <fstream>
 #include "FileModule.h"
 using namespace std;
-/*struct answer
-{
-	char quest[50], v1[15], v2[15], v3[15], v4[15];
-	int k;
-};*/
 
-
-
-void TrainingMode(question cycles[]/*, question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[])*/)
+void TrainingMode(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[])
 {
 	int ch1, answer;
 
@@ -72,7 +65,7 @@ void StudentMenu(question cycles[], question mass[], question stroki[], question
 	cout << "Введите пункт меню: "; cin >> ch;
 	switch (ch)
 	{
-	case 1: TrainingMode(cycles,mass,stroki,recurse,structs,files,adress,dynamic); break;
+	case 1: TrainingMode(cycles, mass, stroki, recurse, structs,files,adress,dynamic); break;
 	case 2: TestingMode(); break;
 	case 3: ExaminationMode(); break;
 	default: cout << "Неверный пункт меню";
