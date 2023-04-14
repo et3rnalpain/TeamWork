86a258e2f997d5319c68908c19bf79a75e81d743
stereob1tch <nikita.dolb@gmail.com>
2023-04-15 01:42:22 +0300

    сделал удаление вопросов +- адекватное и на этом отчаливаю спать

--- Teacher.cpp
+++ Teacher.cpp
@@ -19,21 +19,22 @@ void StudentListChange(user users[])
 	cout << "2. Удалить студента " << endl;
 	cout << "3. Вывести список студентов. " << endl;
 	cout << "Нажмите 0, чтобы выйти" << endl;
-	cin >> ch;
-
-	while (ch < 0 || ch > 3)
-	{
-		system("cls");
-		cout << "Неверный ввод." << endl;
-		cout << "Работа со списком студентов: " << endl;
-		cout << "1. Добавить студента " << endl;
-		cout << "2. Удалить студента " << endl;
-		cout << "3. Вывести список студентов. " << endl;
-		cout << "Нажмите 0, чтобы выйти" << endl;
-		cin >> ch;
-	}
 
 	do {
+		cin >> ch;
+
+		while (ch < 0 || ch > 3)
+		{
+			system("cls");
+			cout << "Неверный ввод." << endl;
+			cout << "Работа со списком студентов: " << endl;
+			cout << "1. Добавить студента " << endl;
+			cout << "2. Удалить студента " << endl;
+			cout << "3. Вывести список студентов. " << endl;
+			cout << "Нажмите 0, чтобы выйти" << endl;
+			cin >> ch;
+		}
+
 		switch (ch)
 		{
 		case 1: {
@@ -103,25 +104,27 @@ void StudentListChange(user users[])
 					users[GetUsersCount("LogsCyphered")].os = 0;
 				}
 			}
-		}
+		}; break;
 
-		case 3: {
+		case 3: 
+		{
 			cout << "Список студентов: " << endl << endl;
 
 			for (int i = 0; i < GetUsersCount("LogsCyphered"); i++)
 			{
-				cout << users[i].Name << users[i].Surname << endl;
+				if (users[i].type == "teacher")
+					continue;
+				cout << users[i].Name << " " << users[i].Surname << endl;
 				cout << "Логин: " << users[i].Login << endl;
 				cout << "Пароль: " << users[i].Password << endl;
-
-				cout << endl;
-
-				cout << "Оценки: " << setw(10) << users[i].o1 << " " << users[i].o2 << " " << users[i].o3 << " " << users[i].o4 << " " << users[i].o5
+				cout << "Оценки: " << users[i].o1 << " " << users[i].o2 << " " << users[i].o3 << " " << users[i].o4 << " " << users[i].o5
 					<< " " << users[i].o6 << " " << users[i].o7 << " " << users[i].o8 << endl;
 				cout << "Средний балл: " << users[i].os << endl;
 				cout << "Оценка за итоговый тест: " << users[i].oi << endl;
+
+				cout << endl;
 			}
-		}
+		}; break;
 		
 
 		}
@@ -130,15 +133,232 @@ void StudentListChange(user users[])
 
 void QuestionDelete(int theme_num, question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[])
 {
+	string quest, var1, var2, var3, var4, answer;
+	int delete_num, delete_count = 0;
 
+	switch (theme_num)
+	{
+
+	case 1:
+	{
+		cout << "Список вопросов: " << endl << endl;
+		for (int i = 0; i < GetQuestionsCount("pasha"); i++)
+		{
+			cout << i + 1 << ". " << cycles[i].quest << endl;
+		}
+
+		do{
+			cout << endl << "Введите номер вопроса, который вы хотите удалить.";
+			cout << endl << "Нажмите 0, чтобы выйти." << endl;
+			cin >> delete_num;
+
+			delete_count++;
+			cycles[delete_num - 1].quest = cycles[GetQuestionsCount("pasha") - delete_count].quest;
+			cycles[delete_num - 1].var1 = cycles[GetQuestionsCount("pasha") - delete_count].var1;
+			cycles[delete_num - 1].var2 = cycles[GetQuestionsCount("pasha") - delete_count].var1;
+			cycles[delete_num - 1].var3 = cycles[GetQuestionsCount("pasha") - delete_count].var3;
+			cycles[delete_num - 1].var4 = cycles[GetQuestionsCount("pasha") - delete_count].var4;
+			cycles[delete_num - 1].answer = cycles[GetQuestionsCount("pasha") - delete_count].answer;
+
+			cycles[GetQuestionsCount("pasha") - delete_count].quest = " ";
+			cycles[GetQuestionsCount("pasha") - delete_count].var1 = " ";
+			cycles[GetQuestionsCount("pasha") - delete_count].var2 = " ";
+			cycles[GetQuestionsCount("pasha") - delete_count].var3 = " ";
+			cycles[GetQuestionsCount("pasha") - delete_count].var4 = " ";
+			cycles[GetQuestionsCount("pasha") - delete_count].answer = " ";
+			
+			system("cls");
+
+			cout << "Редактированный список вопросов: " << endl << endl;
+
+			for (int i = 0; i < GetQuestionsCount("pasha") - delete_count; i++)
+			{
+				cout << i + 1 << ". " << cycles[i].quest << endl;
+			}
+		} while (delete_num != 0);
+
+		
+
+	}; break;
+
+	case 2:
+	{
+		for (int i = 0; i < GetQuestionsCount("2questionsCyphered"); i++)
+		{
+			cout << i + 1 << ". " << mass[i].quest << endl;
+		}
+	}; break;
+
+	}
 }
 
 void QuestionAdd(int theme_num, question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[])
 {
-	fstream f;
-	int ch1;
+	string quest, var1, var2, var3, var4, answer;
 
-	
+	switch (theme_num)
+	{
+	case 1:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		cycles[GetQuestionsCount("1questionsCyphered.txt") + 1].quest = quest;
+		cycles[GetQuestionsCount("1questionsCyphered.txt") + 1].var1 = var1;
+		cycles[GetQuestionsCount("1questionsCyphered.txt") + 1].var2 = var2;
+		cycles[GetQuestionsCount("1questionsCyphered.txt") + 1].var3 = var3;
+		cycles[GetQuestionsCount("1questionsCyphered.txt") + 1].var4 = var4;
+		cycles[GetQuestionsCount("1questionsCyphered.txt") + 1].answer = answer;
+	}; break;
+
+	case 2:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		mass[GetQuestionsCount("2questionsCyphered.txt") + 1].quest = quest;
+		mass[GetQuestionsCount("2questionsCyphered.txt") + 1].var1 = var1;
+		mass[GetQuestionsCount("2questionsCyphered.txt") + 1].var2 = var2;
+		mass[GetQuestionsCount("2questionsCyphered.txt") + 1].var3 = var3;
+		mass[GetQuestionsCount("2questionsCyphered.txt") + 1].var4 = var4;
+		mass[GetQuestionsCount("2questionsCyphered.txt") + 1].answer = answer;
+	}
+
+	case 3:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		stroki[GetQuestionsCount("3questionsCyphered.txt") + 1].quest = quest;
+		stroki[GetQuestionsCount("3questionsCyphered.txt") + 1].var1 = var1;
+		stroki[GetQuestionsCount("3questionsCyphered.txt") + 1].var2 = var2;
+		stroki[GetQuestionsCount("3questionsCyphered.txt") + 1].var3 = var3;
+		stroki[GetQuestionsCount("3questionsCyphered.txt") + 1].var4 = var4;
+		stroki[GetQuestionsCount("3questionsCyphered.txt") + 1].answer = answer;
+	}
+
+	case 4:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		recurse[GetQuestionsCount("4questionsCyphered.txt") + 1].quest = quest;
+		recurse[GetQuestionsCount("4questionsCyphered.txt") + 1].var1 = var1;
+		recurse[GetQuestionsCount("4questionsCyphered.txt") + 1].var2 = var2;
+		recurse[GetQuestionsCount("4questionsCyphered.txt") + 1].var3 = var3;
+		recurse[GetQuestionsCount("4questionsCyphered.txt") + 1].var4 = var4;
+		recurse[GetQuestionsCount("4questionsCyphered.txt") + 1].answer = answer;
+	}
+
+	case 5:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		structs[GetQuestionsCount("5questionsCyphered.txt") + 1].quest = quest;
+		structs[GetQuestionsCount("5questionsCyphered.txt") + 1].var1 = var1;
+		structs[GetQuestionsCount("5questionsCyphered.txt") + 1].var2 = var2;
+		structs[GetQuestionsCount("5questionsCyphered.txt") + 1].var3 = var3;
+		structs[GetQuestionsCount("5questionsCyphered.txt") + 1].var4 = var4;
+		structs[GetQuestionsCount("5questionsCyphered.txt") + 1].answer = answer;
+	}
+
+	case 6:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		files[GetQuestionsCount("6questionsCyphered.txt") + 1].quest = quest;
+		files[GetQuestionsCount("6questionsCyphered.txt") + 1].var1 = var1;
+		files[GetQuestionsCount("6questionsCyphered.txt") + 1].var2 = var2;
+		files[GetQuestionsCount("6questionsCyphered.txt") + 1].var3 = var3;
+		files[GetQuestionsCount("6questionsCyphered.txt") + 1].var4 = var4;
+		files[GetQuestionsCount("6questionsCyphered.txt") + 1].answer = answer;
+	}
+
+	case 7:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		adress[GetQuestionsCount("7questionsCyphered.txt") + 1].quest = quest;
+		adress[GetQuestionsCount("7questionsCyphered.txt") + 1].var1 = var1;
+		adress[GetQuestionsCount("7questionsCyphered.txt") + 1].var2 = var2;
+		adress[GetQuestionsCount("7questionsCyphered.txt") + 1].var3 = var3;
+		adress[GetQuestionsCount("7questionsCyphered.txt") + 1].var4 = var4;
+		adress[GetQuestionsCount("7questionsCyphered.txt") + 1].answer = answer;
+	}
+
+	case 8:
+	{
+		cout << "Введите вопрос: " << endl;
+		cin >> quest;
+		cout << "Введите варианты ответа: " << endl;
+		cout << "1. "; cin >> var1;
+		cout << "2. "; cin >> var2;
+		cout << "3. "; cin >> var3;
+		cout << "4. "; cin >> var4;
+
+		cout << "Введите номер правильного варианта ответа: "; cin >> answer;
+
+		dynamic[GetQuestionsCount("8questionsCyphered.txt") + 1].quest = quest;
+		dynamic[GetQuestionsCount("8questionsCyphered.txt") + 1].var1 = var1;
+		dynamic[GetQuestionsCount("8questionsCyphered.txt") + 1].var2 = var2;
+		dynamic[GetQuestionsCount("8questionsCyphered.txt") + 1].var3 = var3;
+		dynamic[GetQuestionsCount("8questionsCyphered.txt") + 1].var4 = var4;
+		dynamic[GetQuestionsCount("8questionsCyphered.txt") + 1].answer = answer;
+	}
+	}
+
 	return;
 }
 
@@ -179,40 +399,48 @@ void QuestionWork(question cycles[], question mass[], question stroki[], questio
 
 		case 0: return;
 
-		default: {
+		default: 
+		{
 			cout << "Неверный ввод!" << endl;
 		}
 
 		}
-	} while (ch1 != 0);
 
-	cout << "Выберите действие: " << endl;
-	cout << "1. Удалить вопрос" << endl;
-	cout << "2. Добавить вопрос" << endl;
-	cout << "3. Изменить вопрос" << endl;
-	cout << "Нажмите 0, чтобы выйти" << endl;
-	cin >> ch2;
-
-	while (ch2 < 0 || ch2 > 3)
-	{
 		system("cls");
+
+		cout << "Выберите действие: " << endl;
 		cout << "1. Удалить вопрос" << endl;
 		cout << "2. Добавить вопрос" << endl;
 		cout << "3. Изменить вопрос" << endl;
 		cout << "Нажмите 0, чтобы выйти" << endl;
-		cin >> ch2;
-	}
+		
 
-	do {
-		switch (ch2)
-		{
-		case 1: QuestionDelete(theme_num, cycles,mass,stroki,recurse,structs,files,adress,dynamic); break;
-		case 2: QuestionAdd(theme_num, cycles, mass, stroki, recurse, structs, files, adress, dynamic); break;
-		case 3: QuestionChange(theme_num, cycles, mass, stroki, recurse, structs, files, adress, dynamic); break;
-		case 0: return;
-		}
+		do {
+			cin >> ch2;
 
-	} while (ch2 != 0);
+			while (ch2 < 0 || ch2 > 3)
+			{
+				system("cls");
+				cout << "Неверный ввод!" << endl;
+				cout << "Выберите действие: " << endl;
+				cout << "1. Удалить вопрос" << endl;
+				cout << "2. Добавить вопрос" << endl;
+				cout << "3. Изменить вопрос" << endl;
+				cout << "Нажмите 0, чтобы выйти" << endl;
+				cin >> ch2;
+			}
+			switch (ch2)
+			{
+			case 1: {system("cls"); QuestionDelete(theme_num, cycles, mass, stroki, recurse, structs, files, adress, dynamic); break; }
+			case 2: {system("cls"); QuestionAdd(theme_num, cycles, mass, stroki, recurse, structs, files, adress, dynamic); break; }
+			case 3: {system("cls"); QuestionChange(theme_num, cycles, mass, stroki, recurse, structs, files, adress, dynamic); break; }
+			case 0: return;
+			}
+
+		} while (ch2 != 0);
+	} while (ch1 != 0);
+
+	
 }
 
 void TeacherMenu(question cycles[], question mass[], question stroki[], question recurse[], question structs[], question files[], question adress[], question dynamic[], user users[])
